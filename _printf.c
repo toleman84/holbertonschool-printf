12431e5392af138ca1f3e0eb8b7d028aae4efc02
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-07 17:10:06 -0800

    task 0

--- _printf.c
+++ _printf.c
@@ -1,4 +1,52 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+/**
+ * my_printf - Short description.
+ * @format: forst member.
+ * @args: second member.
+ *
+ */
+
+void my_printf(const char *format, va_list args)
+{
+	int state = 0;
+	int i;
+	while (*format)
+	{
+		if (state == 0)
+		{
+			if (*format == '%')
+				state = 1;
+			else
+				putchar(*format);
+		}
+		else if (state == 1)
+		{
+			switch (*format)
+			{
+				case 'c':
+				{
+					char ch = va_arg(args, int);
+					putchar(ch);
+					break;
+				}
+				case 's':
+				{
+					const char *s = va_arg(args, const char *);
+					while (*s)
+					{
+						putchar(*s++);
+					}
+					break;
+				}
+			}
+			state = 0;
+		}
+		format++;
+	}
+}
 
 /**
  * _printf - Short description.
@@ -9,39 +57,24 @@
 
 int _printf(const char *format, ...)
 {
-	char *tole = NULL;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	va_list args;
 
-	va_list ap;
+	va_start(args, format);
 
-	va_start(ap, format);
+	my_printf(format, args);
 
-	while (format && format[j])
-		j++;
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case ('c'):
-				*tole = (va_arg(ap, int));
-			break;
+	va_end(args);
+	return (0);
+}
 
-			case ('s'):
-				tole = (va_arg(ap, char *));
-			break;
+int main(void)
+{
+	_printf("print a character %c\n", 'H');
+	_printf("print a string %s\n", "Hello world");
+	_printf("print an interger %d\n", 10);
+	_printf("print a neg interger %d\n", -10);
+	_printf("print a zero interger %d\n", 0);
 
-			case ('d'):
-				*tole = (va_arg(ap, int));
-			break;
-
-			case ('i'):
-				*tole = (va_arg(ap, int));
-			break;
-		}
-		i++;
-	}
-	va_end(ap);
 	return (0);
 }
 
