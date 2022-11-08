6fa9c14b8a7602ec97cfba924e883a8ba97528bd
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-07 17:33:52 -0800

    task 0

--- _printf.c
+++ _printf.c
@@ -12,6 +12,7 @@
 void my_printf(const char *format, va_list args)
 {
 	int state = 0;
+
 	while (*format)
 	{
 		if (state == 0)
@@ -26,14 +27,17 @@ void my_printf(const char *format, va_list args)
 			switch (*format)
 			{
 				case 'c':
+
 				{
 					char ch = va_arg(args, int);
+
 					putchar(ch);
 					break;
 				}
 				case 's':
 				{
 					const char *s = va_arg(args, const char *);
+
 					while (*s)
 					{
 						putchar(*s++);
