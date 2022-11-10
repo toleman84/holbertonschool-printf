d7db8d7a9b55e34eedfad3a31000854923161638
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-10 07:21:47 -0800

    Function that contains the printf flags

--- flags.c
+++ flags.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * printflags - Function that contains the printf flags.
- * @format: String that printflags takes.
+ * printflags - Function that contains the printf flags
+ * @format: String that printflags takes
  *
  * Return: Always 0 (Success)
  */
@@ -13,10 +13,13 @@ int (*printflags(const char format))(va_list)
 		{'c', print_char},
 		{'s', print_str},
 		{'%', print_perc},
+		/**{'i'}, print_int*/
 		{'\0', NULL}
 	};
 	int j;
 
+	if (format)
+	{
 		for (j = 0; j < 6; j++)
 		{
 			if (format == func[j].c)
@@ -24,6 +27,24 @@ int (*printflags(const char format))(va_list)
 				return (func[j].f);
 			}
 		}
+		_putchar('%');
+		_putchar(format);
+		return (border_case);
+	}
+	else
+		return (NULL);
 	return (0);
+}
+
+/**
+ * border_case - Function that return 2
+ * @ap: The arguments.
+ *
+ * Return: 2.
+ */
+
+int border_case(__attribute__((unused))va_list ap)
+{
+	return (2);
 }
 
